dc4e135cb5fb950761e18e318b4d289a20ffb8f9
César Lima <cesar@hotglue.io>
2026-01-21 12:09:05 -0300

    HGI-9563: made sure that the client retries 429 errors

--- target_nationbuilder/client.py
+++ target_nationbuilder/client.py
@@ -62,6 +62,8 @@ class NationBuilderSink(HotglueSink):
             raise FatalAPIError(msg)
         elif "cf-error-details" in response.text:
             resp_text = extract_text_from_html(response.text)
+            if response.status_code == 429:
+                raise RetriableAPIError(resp_text, response)
             raise FatalAPIError(resp_text)
         elif response.status_code in [429] or 500 <= response.status_code < 600:
             msg = self.response_error_message(response)
